3852af48a4f94294d2055df490a216a0fa385d6b
geovania-prog <geovaniasc@gmail.com>
2024-10-16 11:41:40 -0300

    adicionando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './estilo.css'
+ 
+import biscoito from './assets/biscoito.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = ['Siga os bons e aprenda com eles.','o bom-senso vale mais do que muito conhecimento.',
+      'O riso e a menor distancia entre duas pessoas.',
+      'Deixe de lado as preocupaçoes e seja feliz.',
+      'Realize o obvio, pense no improvavel e conquiste o impossivel.',
+      'Acredite em milagres, mas nao dependa deles.',
+      'A maior barreira para o sucesso e o medo do fracasso.'];
+
+  }
+
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = ' "' + this.frases[numeroAleatorio] + ' "'  }
+  this.setState(state);
+
+render(){
+  return(
+    <div className='container'>
+      <img src={biscoito} className='img' />
+      <botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
+      <h3 className='textoFrase'>{this.state.textoFase}</h3>
     </div>
+
   );
+}
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn} >{this.props.nome}</button>
+
+      </div>
+    );
+  }
 }
 
 export default App;
